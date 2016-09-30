bf21c6450e0b54cc8e2cd21ba5e631b3dcbf6d8c
Nathaniel Alba <nathaniel.alba@gmail.com>
2016-09-30 00:45:00 -0700

    minor fix

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 
 const App = () => {
   return (
-    <View />
+    <View>
+      <Text>
+        Hello There
+      </Text>
+    </View>
   );
 };
 
